227c51034a8657b75ec4bc5cc65ab2a57f04097e
Drego31 <dominik.betka@arbuz.team>
2019-06-14 00:29:05 +0200

    exclude products from store

---
+++ src/store/products.js
@@ -0,0 +1,51 @@
+import Image from '@/assets/main-photo.png'
+
+export default {
+  namespaced: true,
+  state: {
+    list: [
+      {
+        id: 1,
+        img: Image,
+        title: 'Title',
+        shortDescription: 'Description',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ' +
+          'elementum lacus a est pretium pellentesque. Praesent vel ante sagittis, dignissim ' +
+          'odio at, finibus leo. Sed ultricies, arcu sit amet volutpat malesuada, massa quam ' +
+          'cursus neque, sit amet vehicula eros tellus in nunc. Suspendisse blandit odio in ' +
+          'lorem scelerisque tristique. In bibendum sit amet tortor in porta. Nullam tempus ' +
+          'facilisis mi id maximus.',
+      },
+      {
+        id: 2,
+        img: Image,
+        title: 'Title',
+        shortDescription: 'Description',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ' +
+          'elementum lacus a est pretium pellentesque. Praesent vel ante sagittis, dignissim ' +
+          'odio at, finibus leo. Sed ultricies, arcu sit amet volutpat malesuada, massa quam ' +
+          'cursus neque, sit amet vehicula eros tellus in nunc. Suspendisse blandit odio in ' +
+          'lorem scelerisque tristique. In bibendum sit amet tortor in porta. Nullam tempus ' +
+          'facilisis mi id maximus.',
+      },
+      {
+        id: 3,
+        img: Image,
+        title: 'Title',
+        shortDescription: 'Description',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ' +
+          'elementum lacus a est pretium pellentesque. Praesent vel ante sagittis, dignissim ' +
+          'odio at, finibus leo. Sed ultricies, arcu sit amet volutpat malesuada, massa quam ' +
+          'cursus neque, sit amet vehicula eros tellus in nunc. Suspendisse blandit odio in ' +
+          'lorem scelerisque tristique. In bibendum sit amet tortor in porta. Nullam tempus ' +
+          'facilisis mi id maximus.',
+      },
+    ],
+  },
+  getters: {
+    articleById: state => id => state.list.find(article => article.id === Number(id)),
+  },
+  mutations: {
+    setSome: state => state.some = true,
+  },
+}
